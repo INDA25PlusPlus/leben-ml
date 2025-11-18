316cf8ee106b71f23e81595b40d7f2287100da3d
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-11-18 21:07:44 +0100

    Refactor to one common `main.cpp`, and use preprocessor define flag to differentiate between cpu only and cuda mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 // Created by Leonard on 2025-11-16.
 //
 
-#include <iostream>
-
 #include "common/entrypoint.hpp"
 
 int main() {
--- src/main.cu
+++
@@ -1,9 +0,0 @@
-//
-// Created by Leonard on 2025-11-16.
-//
-
-#include <iostream>
-
-int main() {
-    return entrypoint::main();
-}
