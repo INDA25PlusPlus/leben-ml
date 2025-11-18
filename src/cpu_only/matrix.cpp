277e1eb5befc4b4e2a0c2784b0407f7009d47026
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-11-18 21:06:34 +0100

    Working towards #4. Add `mult_add` and `mult_add_vec`

--- src/cpu_only/matrix.cpp
+++ src/cpu_only/matrix.cpp
@@ -4,6 +4,8 @@
 
 #include "matrix.hpp"
 
+#include <cmath>
+
 void matrix::mult(
     matrix_float_t const *const a,
     matrix_float_t const *const b,
@@ -13,7 +15,7 @@ void matrix::mult(
     std::size_t const p)
 {
     // how grueling this is
-    // the poor cpu is slow
+    // the poor cpu so slow
     // unlike tensor cores
 
     std::size_t ij_ij = 0;
@@ -42,6 +44,60 @@ void matrix::add(
     for (auto i = 0; i < m; i++) {
         for (auto j = 0; j < n; j++) {
             c[ij_ij] = a[ij_ij] + b[ij_ij];
+            ij_ij++;
+        }
+    }
+}
+
+void matrix::mult_add(
+    matrix_float_t const *const a,
+    matrix_float_t const *const b,
+    matrix_float_t const *const c,
+    matrix_float_t *const d,
+    size_t const m,
+    size_t const n,
+    size_t const p)
+{
+    std::size_t ij_ij = 0;
+    for (auto i = 0; i < m; i++) {
+        std::size_t const ik_i0 = n * i;
+        for (auto j = 0; j < p; j++) {
+            matrix_float_t accum = 0;
+            std::size_t const kj_0j = n * j;
+            for (auto k = 0; k < n; k++) {
+                matrix_float_t const a_ik = a[ik_i0 + k];
+                matrix_float_t const b_kj = b[kj_0j + k];
+                accum += a_ik * b_kj;
+            }
+            matrix_float_t const c_ij = c[ij_ij];
+            d[ij_ij] = accum + c_ij;
+            ij_ij++;
+        }
+    }
+}
+
+void matrix::mult_add_vec(
+    matrix_float_t const *const a,
+    matrix_float_t const *const b,
+    matrix_float_t const *const c,
+    matrix_float_t *const d,
+    size_t const m,
+    size_t const n,
+    size_t const p)
+{
+    std::size_t ij_ij = 0;
+    for (auto i = 0; i < m; i++) {
+        std::size_t const ik_i0 = n * i;
+        for (auto j = 0; j < p; j++) {
+            matrix_float_t accum = 0;
+            std::size_t const kj_0j = n * j;
+            for (auto k = 0; k < n; k++) {
+                matrix_float_t const a_ik = a[ik_i0 + k];
+                matrix_float_t const b_kj = b[kj_0j + k];
+                accum += a_ik * b_kj;
+            }
+            matrix_float_t const c_j = c[j];
+            d[ij_ij] = accum + c_j;
             ij_ij++;
         }
     }
--- src/cpu_only/matrix.hpp
+++ src/cpu_only/matrix.hpp
@@ -9,6 +9,8 @@
 
 namespace matrix {
     /**
+     * C = AB
+     *
      * Assumes c is zero-initialized.
      *
      * All matrix values are stored with the second index changing the fastest,
@@ -30,6 +32,8 @@ namespace matrix {
         std::size_t p);
 
     /**
+     * C = A + B
+     *
      * Assumes c is zero-initialized.
      *
      * All matrix values are stored with the second index changing the fastest,
@@ -47,4 +51,56 @@ namespace matrix {
         matrix_float_t *c,
         std::size_t m,
         std::size_t n);
+
+    /**
+     * D = AB + C
+     *
+     * d does not have to be initialized.
+     *
+     * All matrix values are stored with the second index changing the fastest,
+     * e.g. A = {a_11, a_12, a_21, a_22}.
+     *
+     * @param a an m × n matrix
+     * @param b an n × p matrix
+     * @param c an m × p matrix
+     * @param d an m × p matrix, AB + C
+     * @param m the number of rows of a and c
+     * @param n the number of columns of a, and rows of b
+     * @param p the number of columns of b and c
+     */
+    void mult_add(
+        matrix_float_t const *a,
+        matrix_float_t const *b,
+        matrix_float_t const *c,
+        matrix_float_t *d,
+        size_t m,
+        size_t n,
+        size_t p);
+
+    /**
+     * D = AB + C
+     *
+     * Sets D to the product of A and B, and adds the row vector C to each row.
+     *
+     * d does not have to be initialized.
+     *
+     * All matrix values are stored with the second index changing the fastest,
+     * e.g. A = {a_11, a_12, a_21, a_22}.
+     *
+     * @param a an m × n matrix
+     * @param b an n × p matrix
+     * @param c a 1 × p matrix
+     * @param d an m × p matrix, AB + C
+     * @param m the number of rows of a
+     * @param n the number of columns of a, and rows of b
+     * @param p the number of columns of b and c
+     */
+    void mult_add_vec(
+        matrix_float_t const *a,
+        matrix_float_t const *b,
+        matrix_float_t const *c,
+        matrix_float_t *d,
+        size_t m,
+        size_t n,
+        size_t p);
 }
